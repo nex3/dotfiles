7a3ab32b9d3d37fd1a360d48582dacdf4c90017c
Nathan Weizenbaum <nex342@gmail.com>
2008-11-05 12:38:40 -0800

    Emacs22 compat.

--- emacs.el
+++ emacs.el
@@ -10,6 +10,24 @@
   (tool-bar-mode -1))
 (menu-bar-mode -1)
 
+(if (version< emacs-version "23")
+  (defmacro with-selected-frame (frame &rest body)
+    "Execute the forms in BODY with FRAME as the selected frame.
+The value returned is the value of the last form in BODY.
+See also `with-temp-buffer'."
+    (declare (indent 1) (debug t))
+    (let ((old-frame (make-symbol "old-frame"))
+          (old-buffer (make-symbol "old-buffer")))
+      `(let ((,old-frame (selected-frame))
+             (,old-buffer (current-buffer)))
+         (unwind-protect
+             (progn (select-frame ,frame)
+                    ,@body)
+           (if (frame-live-p ,old-frame)
+               (select-frame ,old-frame))
+           (if (buffer-live-p ,old-buffer)
+               (set-buffer ,old-buffer)))))))
+
 (defun init-frame (&optional frame)
   "Initialize FRAME with my preferences."
   (setq frame (or frame (selected-frame)))
