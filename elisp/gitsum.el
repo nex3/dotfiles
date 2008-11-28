7eb09af8c06d69ed538ade845c11b53cc634f5d5
Nathan Weizenbaum <nex342@gmail.com>
2008-11-27 23:31:28 -0800

    Update gitsum.

--- elisp/gitsum.el
+++ elisp/gitsum.el
@@ -11,10 +11,17 @@
 
 (eval-when-compile (require 'cl))
 
+(defcustom gitsum-reuse-buffer t
+  "Whether `gitsum' should try to reuse an existing buffer
+if there is already one that displays the same directory."
+  :group 'git
+  :type 'boolean)
+
 (easy-mmode-defmap gitsum-diff-mode-shared-map
   '(("A" . gitsum-amend)
     ("c" . gitsum-commit)
     ("g" . gitsum-refresh)
+    ("k" . gitsum-kill-dwim)
     ("P" . gitsum-push)
     ("R" . gitsum-revert)
     ("s" . gitsum-switch-to-git-status)
@@ -25,6 +32,7 @@
 (define-derived-mode gitsum-diff-mode diff-mode "gitsum"
   "Git summary mode is for preparing patches to a Git repository.
 This mode is meant to be activated by `M-x gitsum' or pressing `s' in git-status.
+\\{gitsum-diff-mode-shared-map}
 \\{gitsum-diff-mode-map}"
   ;; magic...
   (lexical-let ((ro-bind (cons 'buffer-read-only gitsum-diff-mode-shared-map)))
@@ -37,7 +45,7 @@ This mode is meant to be activated by `M-x gitsum' or pressing `s' in git-status
 
 ;; When git.el is loaded, hack into keymap.
 (when (boundp 'git-status-mode-map)
-  (define-key git-status-mode-map "s" 'gitsum))
+  (define-key git-status-mode-map "s" 'gitsum-switch-from-git-status))
 
 ;; Undo doesn't work in read-only buffers else.
 (defun gitsum-undo ()
@@ -49,7 +57,7 @@ A numeric argument serves as a repeat count."
   (let ((inhibit-read-only t))
     (undo)))
 
-(defun gitsum-refresh ()
+(defun gitsum-refresh (&optional arguments)
   "Regenerate the patch based on the current state of the index."
   (interactive)
   (let ((inhibit-read-only t))
@@ -57,21 +65,40 @@ A numeric argument serves as a repeat count."
     (insert "# Directory:  " default-directory "\n")
     (insert "# Use n and p to navigate and k to kill a hunk.  u is undo, g will refresh.\n")
     (insert "# Edit the patch as you please and press 'c' to commit.\n\n")
-    (let ((diff (shell-command-to-string "git diff")))
+    (let ((diff (shell-command-to-string (concat "git diff " arguments))))
       (if (zerop (length diff))
           (insert "## No changes. ##")
         (insert diff)
         (goto-char (point-min))
         (delete-matching-lines "^index \\|^diff --git ")))
-    (set-buffer-modified-p nil)))
+    (set-buffer-modified-p nil)
+    (goto-char (point-min))
+    (forward-line 4)))
+
+(defun gitsum-kill-dwim ()
+  "Kill the current hunk or file depending on point."
+  (interactive)
+  (let ((inhibit-read-only t))
+    (if (looking-at "^---\\|^\\+\\+\\+")
+        (diff-file-kill)
+      (diff-hunk-kill)
+      (save-excursion
+        (when (or (looking-at "^--- ")
+                  (eobp))
+          (let ((here (point)))
+            (forward-line -2)
+            (when (looking-at "^--- ")
+              (delete-region here (point)))))))))
 
 (defun gitsum-commit ()
   "Commit the patch as-is, asking for a commit message."
   (interactive)
   (shell-command-on-region (point-min) (point-max) "git apply --check --cached")
-  (let ((buffer (get-buffer-create "*gitsum-commit*")))
+  (let ((buffer (get-buffer-create "*gitsum-commit*"))
+        (dir default-directory))
     (shell-command-on-region (point-min) (point-max) "(cat; git diff --cached) | git apply --stat" buffer)
     (with-current-buffer buffer
+      (setq default-directory dir)
       (goto-char (point-min))
       (insert "\n")
       (while (re-search-forward "^" nil t)
@@ -127,7 +154,8 @@ A numeric argument serves as a repeat count."
 (defun gitsum-kill-buffer ()
   "Kill the current buffer if it has no manual changes."
   (interactive)
-  (unless (buffer-modified-p)
+  (if (buffer-modified-p)
+      (message "Patch was modified, use C-x k to kill.")
     (kill-buffer nil)))
 
 (defun gitsum-switch-to-git-status ()
@@ -135,11 +163,46 @@ A numeric argument serves as a repeat count."
   (interactive)
   (git-status default-directory))
 
+(defun gitsum-switch-from-git-status ()
+  "Switch to gitsum, resticting diff to marked files if any."
+  (interactive)
+  (let ((marked (git-get-filenames
+                 (ewoc-collect git-status
+                               (lambda (info) (git-fileinfo->marked info))))))
+    (gitsum)
+    (when marked
+      (gitsum-refresh (mapconcat 'identity marked " ")))))
+
+(defun gitsum-find-buffer (dir)
+  "Find the gitsum buffer handling a specified directory."
+  (let ((list (buffer-list))
+        (fulldir (expand-file-name dir))
+        found)
+    (while (and list (not found))
+      (let ((buffer (car list)))
+        (with-current-buffer buffer
+          (when (and list-buffers-directory
+                     (string-equal fulldir
+                                   (expand-file-name list-buffers-directory))
+                     (eq major-mode 'gitsum-diff-mode))
+            (setq found buffer))))
+      (setq list (cdr list)))
+    found))
+
+;;;###autoload
 (defun gitsum ()
   "Entry point into gitsum-diff-mode."
   (interactive)
-  (switch-to-buffer (generate-new-buffer "*gitsum*"))
-  (gitsum-diff-mode)
-  (gitsum-refresh))
+  (let* ((dir default-directory)
+         (buffer (or (and gitsum-reuse-buffer (gitsum-find-buffer dir))
+                     (generate-new-buffer "*gitsum*"))))
+    (switch-to-buffer buffer)
+    (gitsum-diff-mode)
+    (set (make-local-variable 'list-buffers-directory) dir)
+    (gitsum-refresh)))
+
+;; viper compatible
+(eval-after-load "viper"
+  '(add-to-list 'viper-emacs-state-mode-list 'gitsum-diff-mode))
 
 (provide 'gitsum)
