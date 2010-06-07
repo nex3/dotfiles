41ab030e565bdae412b72e8ea7d43f8b4f6da054
Nathan Weizenbaum <nex342@gmail.com>
2010-06-07 13:23:36 -0700

    [Emacs] Make my-magit-status prompt for a directory if necessary.

--- elisp/my-magit.el
+++ elisp/my-magit.el
@@ -18,14 +18,20 @@ one Magit status buffers active, this returns an arbitrary one."
       (return-from nil buffer))))
 
 ;;;###autoload
-(defun my-magit-status ()
+(defun my-magit-status (dir)
   "Like `magit-status', but use this perspective's Magit buffer if possible.
+DIR is the Git-managed directory for which to set up the Magit buffer.
 
-Uses `my-magit-status-buffer' to find the correct status buffer."
-  (interactive)
-  (let ((buffer (my-magit-current-buffer)))
-    (magit-status (if buffer (buffer-local-value 'default-directory buffer)
-                    (magit-get-top-dir default-directory)))))
+When called interactively, use `my-magit-status-buffer' to find
+the correct status buffer.  Fall back on the default directory or
+simply prompting the user."
+  (interactive (list
+                (let ((buffer (my-magit-current-buffer)))
+                  (or (if buffer (buffer-local-value 'default-directory buffer))
+                      (magit-get-top-dir default-directory)
+                      (magit-read-top-dir (and (consp current-prefix-arg)
+                                               (> (car current-prefix-arg) 4)))))))
+  (magit-status dir))
 
 (provide 'my-magit)
 
