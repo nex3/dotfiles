80fe06516fa3ba1a5e27f471eb64ba2151b401b9
Nathan Weizenbaum <nex342@gmail.com>
2013-03-08 12:06:56 -0800

    Add marmalade version 0.0.4.

---
+++ elisp/elpa/furl-0.0.2/furl-autoloads.el
@@ -0,0 +1,18 @@
+;;; furl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("furl-pkg.el" "furl.el") (20794 17627
+;;;;;;  433200))
+
+;;;***
+
+(provide 'furl-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; furl-autoloads.el ends here
