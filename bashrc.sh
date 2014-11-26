1cdd98729e5f0c462ce7f990f85837f836d954b5
Natalie Weizenbaum <nweiz@google.com>
2014-11-26 12:48:24 -0800

    [Bashrc] Update PATH.

--- bashrc.sh
+++ bashrc.sh
@@ -176,7 +176,7 @@ function refresh-path {
     PATH=$original_path
 
     if [ -e $HOME/gems ]; then PATH=$HOME/gems/bin:$PATH; fi
-    PATH=/usr/local/heroku/bin:/var/lib/gems/1.8/bin/:/usr/local/bin:$PATH
+    PATH=/var/lib/gems/1.8/bin/:/usr/local/bin:/usr/local/heroku/bin:$PATH:$HOME/.rvm/bin:$HOME/.pub-cache/bin
 
     for bindir in $HOME/bin/*; do
         if [[ -d "$bindir" && ! ":$PATH" =~ ":$bindir" ]]; then
