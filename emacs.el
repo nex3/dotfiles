77eb9ce3b55ef927bc01950825f63f095c9e6831
Natalie Weizenbaum <nex342@gmail.com>
2016-08-09 13:23:26 -0700

    [Emacs] Add Melpa as a package source.

--- emacs.el
+++ emacs.el
@@ -64,7 +64,8 @@
 (require 'package)
 
 (when (boundp 'package-archives)
-  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/")))
+  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+  (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/")))
 (setq package-user-dir "~/.elisp/elpa")
 (package-initialize)
 
