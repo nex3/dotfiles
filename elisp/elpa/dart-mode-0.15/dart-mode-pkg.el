ce2920148f366f28b050d627086a074270f3883f
Natalie Weizenbaum <nex342@gmail.com>
2017-06-02 17:39:24 -0700

    [Emacs] Add dart-mode version 0.15.

---
+++ elisp/elpa/dart-mode-0.15/dart-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dart-mode" "0.15" "Major mode for editing Dart files" '((cl-lib "0.5") (dash "2.10.0") (flycheck "0.23")) :commit "e6635b390235cf16a8081763768cf925ca2d9133" :url "http://code.google.com/p/dart-mode" :keywords '("language"))
