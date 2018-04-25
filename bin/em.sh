a23aa801874ea04671de6471cddd0933e707e645
Natalie Weizenbaum <nweiz@google.com>
2018-04-25 13:39:08 -0700

    [Bin] Allow em to open a buffer from stdin

--- bin/em.sh
+++ bin/em.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
-if [ "$TERM" = screen ]; then
-    emacsclient -t -a 'temacs' "$@"
-elif [ ! -z "$INSIDE_EMACS" ]; then
-    emacsclient -a 'emacs' "$@"
+
+if [ ${#@} -eq 0 ]; then
+    tempfile=$(mktemp emacs-stdin-$USER.XXXXXXX --tmpdir)
+    cat - > "$tempfile"
+    args="$tempfile"
 else
-    emacsclient -c -a 'emacs' "$@"
+    args="$@"
 fi
+
+if [ "$TERM" = screen ]; then
+    emacsclient -t -a 'temacs' "$args"
+elif [ ! -z "$INSIDE_EMACS" ]; then
+    emacsclient -a 'emacs' "$args"
+else
+    emacsclient -c -a 'emacs' "$args"
+fi
+
+code=$?
+if [ ! -z "$tempfile" ]; then
+    rm -f "$tempfile"
+fi
+exit $code
