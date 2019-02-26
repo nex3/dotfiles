c464e870e1f178158503d63f54068bde53960154
Natalie Weizenbaum <nweiz@google.com>
2019-02-25 17:38:44 -0800

    [Emacs] Add protobuf-mode version 3.7.0.-1.2.

---
+++ elisp/elpa/protobuf-mode-3.7.0pre2/protobuf-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "protobuf-mode" "3.7.0pre2" "major mode for editing protocol buffers." 'nil :commit "776b3a8b11fd243d709ef726ac963aa7d42e532e" :keywords '("google" "protobuf" "languages") :authors '(("Alexandre Vassalotti" . "alexandre@peadrop.com")) :maintainer '("Alexandre Vassalotti" . "alexandre@peadrop.com"))
