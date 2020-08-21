318197df71105d6fe928f0285a72417269fc9539
Natalie Weizenbaum <nweiz@google.com>
2020-08-21 15:55:03 -0700

    [Bash] Fix a bug in "dt"

--- bin/dt.sh
+++ bin/dt.sh
@@ -2,7 +2,7 @@
 
 socket="$(emacsclient -e 'server-socket-dir' 2>/dev/null)/socket"
 if [[ $? == 0 ]]; then
-    ssh -o -R "$socket:$socket" nweiz@nweiz1.sea.corp.google.com
+    ssh -R "$socket:$socket" nweiz@nweiz1.sea.corp.google.com
 else
     ssh nweiz@nweiz1.sea.corp.google.com
 fi
