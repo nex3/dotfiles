c30579956ee8db74902eaf06b89f0d082f19e21a
Natalie Weizenbaum <nweiz@google.com>
2020-11-09 13:40:53 -0800

    [Bash] Fix another bug in "dt"

--- bin/dt.sh
+++ bin/dt.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-socket="$(emacsclient -e 'server-socket-dir' 2>/dev/null)/socket"
-if [[ $? == 0 ]]; then
-    ssh -R "$socket:$socket" nweiz@nweiz1.sea.corp.google.com
+socket="/tmp/emacs$UID/server"
+if [[ -e "$socket" ]]; then
+    ssh -t -o StreamLocalBindUnlink=yes \
+        -R "/tmp/emacs-ssh-server:$socket" \
+        nweiz@nweiz1.sea.corp.google.com \
+        'mkdir "/tmp/emacs$UID"; mv /tmp/emacs-ssh-server "/tmp/emacs$UID/server"; bash'
 else
     ssh nweiz@nweiz1.sea.corp.google.com
 fi
