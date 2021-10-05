dfe454e9e48a437e1d2aa299920801cc021dd1c1
Natalie Weizenbaum <nweiz@google.com>
2021-10-05 15:29:00 -0700

    [Bash] Support linuxbrew

--- bashrc.sh
+++ bashrc.sh
@@ -250,3 +250,9 @@ fi
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+if [ -f /bin/brew ]; then
+    eval "$(/bin/brew shellenv)"
+elif [ -f ~/.linuxbrew/bin/brew ]; then
+    eval "$(~/.linuxbrew/bin/brew shellenv)"
+fi
