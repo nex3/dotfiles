59875a8f8b45c9645dac509ac566f4bf5d591442
Natalie Weizenbaum <nweiz@google.com>
2022-08-02 18:33:24 -0700

    [Elisp] Update Magit

--- elisp/elpa/git-commit-2.10.3/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "2.10.3" "Edit Git commit messages" '((emacs "24.4") (dash "2.13.0") (with-editor "2.5.10")) :commit "d5b747473ab0bf0f0034fca2f57f6497d476b67e" :url "https://github.com/magit/magit" :keywords '("git" "tools" "vc"))
