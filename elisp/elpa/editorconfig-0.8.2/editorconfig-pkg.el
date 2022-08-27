64809bfa6124fd05353eeb99af480dd346c15676
Natalie Weizenbaum <nweiz@google.com>
2022-08-26 18:32:08 -0700

    [Emacs] Add editorconfig version 0.8.2.

---
+++ elisp/elpa/editorconfig-0.8.2/editorconfig-pkg.el
@@ -0,0 +1,12 @@
+(define-package "editorconfig" "0.8.2" "EditorConfig Emacs Plugin"
+  '((cl-lib "0.5")
+    (nadvice "0.3")
+    (emacs "24"))
+  :commit "375418d1d63f6ec780876593181b6d39054919da" :authors
+  '(("EditorConfig Team" . "editorconfig@googlegroups.com"))
+  :maintainer
+  '("EditorConfig Team" . "editorconfig@googlegroups.com")
+  :url "https://github.com/editorconfig/editorconfig-emacs#readme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
