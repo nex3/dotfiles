02e95a92bab89473b2e271f168ce41e4d33dbefb
Natalie Weizenbaum <nweiz@google.com>
2025-10-23 15:48:07 -0700

    [Emacs] Add lua-mode version 20210802.

---
+++ elisp/elpa/lua-mode-20210802/lua-mode-pkg.el
@@ -0,0 +1,24 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "lua-mode" "20210802"
+  "A major-mode for editing Lua scripts."
+  '((emacs "24.3"))
+  :url "http://immerrr.github.com/lua-mode"
+  :commit "7ee3febab01aeacfd33357124e7f605e1e110d99"
+  :revdesc "v20210802-0-g7ee3febab01a"
+  :keywords '("languages" "processes" "tools")
+  :authors '(("2011-2013 immerrr" . "immerrr+lua@gmail.com")
+             ("2010-2011 Reuben Thomas" . "rrt@sc3d.org")
+             ("2006 Juergen Hoetzel" . "juergen@hoetzel.info")
+             ("2001 Christian Vogler" . "cvogler@gradient.cis.upenn.edu")
+             ("1997 Bret Mogilefsky starting from" . "mogul-lua@gelatinous.com")
+             ("tcl-mode by Gregor Schmid" . "schmid@fb3-s7.math.tu-berlin.de")
+             ("Paul Du Bois and" . "pld-lua@gelatinous.com")
+             ("Aaron Smith" . "aaron-lua@gelatinous.com"))
+  :maintainers '(("2011-2013 immerrr" . "immerrr+lua@gmail.com")
+                 ("2010-2011 Reuben Thomas" . "rrt@sc3d.org")
+                 ("2006 Juergen Hoetzel" . "juergen@hoetzel.info")
+                 ("2001 Christian Vogler" . "cvogler@gradient.cis.upenn.edu")
+                 ("1997 Bret Mogilefsky starting from" . "mogul-lua@gelatinous.com")
+                 ("tcl-mode by Gregor Schmid" . "schmid@fb3-s7.math.tu-berlin.de")
+                 ("Paul Du Bois and" . "pld-lua@gelatinous.com")
+                 ("Aaron Smith" . "aaron-lua@gelatinous.com")))
