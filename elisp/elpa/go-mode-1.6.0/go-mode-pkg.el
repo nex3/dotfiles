e35cd3663cb354c5d33d43554a9a79fff2e0f8b6
Natalie Weizenbaum <nweiz@google.com>
2025-10-23 16:45:07 -0700

    [Emacs] Add go-mode version 1.6.0.

---
+++ elisp/elpa/go-mode-1.6.0/go-mode-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "go-mode" "1.6.0"
+  "Major mode for the Go programming language."
+  '((emacs "26.1"))
+  :url "https://github.com/dominikh/go-mode.el"
+  :commit "3273fcece5d9ab7edd4f15b2d6bce61f4e5a0666"
+  :revdesc "v1.6.0-0-g3273fcece5d9"
+  :keywords '("languages" "go"))
