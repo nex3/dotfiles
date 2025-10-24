4320a4d5e6eede107870919061d50b2ef6c22b55
Natalie Weizenbaum <nweiz@google.com>
2025-10-23 17:25:16 -0700

    [Emacs] Add rust-mode version 1.0.6.

---
+++ elisp/elpa/rust-mode-1.0.6/rust-mode-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "rust-mode" "1.0.6"
+  "A major-mode for editing Rust source code."
+  '((emacs "25.1"))
+  :url "https://github.com/rust-lang/rust-mode"
+  :commit "527f8ff2b727d1c027965491ac88230bf2af24ed"
+  :revdesc "1.0.6-0-g527f8ff2b727"
+  :keywords '("languages")
+  :authors '(("Mozilla" . "rust-mode@noreply.github.com"))
+  :maintainers '(("Mozilla" . "rust-mode@noreply.github.com")))
