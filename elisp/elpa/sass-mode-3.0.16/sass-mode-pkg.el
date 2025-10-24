e608d4800842549a55159cb9286cae7418e6fd7d
Natalie Weizenbaum <nweiz@google.com>
2025-10-23 17:18:22 -0700

    [Emacs] Add ssass-mode version 0.2.

--- elisp/elpa/sass-mode-3.0.16/sass-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "sass-mode" "3.0.16" "Major mode for editing Sass files" '((haml-mode "3.0.15")))
---
+++ elisp/elpa/ssass-mode-0.2/ssass-mode-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "ssass-mode" "0.2"
+  "Edit Sass without a Turing Machine."
+  '((emacs "24.3"))
+  :url "http://github.com/AdamNiederer/ssass-mode"
+  :commit "5f36a169a1ad497f1d7a465be386ffb5e1f80bcf"
+  :revdesc "0.2-0-g5f36a169a1ad"
+  :keywords '("languages" "sass")
+  :authors '(("Adam Niederer" . "adam.niederer@gmail.com"))
+  :maintainers '(("Adam Niederer" . "adam.niederer@gmail.com")))
