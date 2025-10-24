07a4628c3adc61a4b40c8bf2e7b03fbc0daef504
Natalie Weizenbaum <nweiz@google.com>
2025-10-23 17:19:49 -0700

    [Emacs] Add tide version 5.1.3.

---
+++ elisp/elpa/tide-5.1.3/tide-pkg.el
@@ -0,0 +1,14 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "tide" "5.1.3"
+  "Typescript Interactive Development Environment."
+  '((emacs    "25.1")
+    (dash     "2.10.0")
+    (s        "1.11.0")
+    (flycheck "27")
+    (cl-lib   "0.5"))
+  :url "http://github.com/ananthakumaran/tide"
+  :commit "bb09bccf070c1915ca8dbaaaaee4e9e3d7ab2ec7"
+  :revdesc "bb09bccf070c"
+  :keywords '("typescript")
+  :authors '(("Anantha kumaran" . "ananthakumaran@gmail.com"))
+  :maintainers '(("Anantha kumaran" . "ananthakumaran@gmail.com")))
--- elisp/elpa/typescript-mode-0.4/typescript-mode-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "typescript-mode" "0.4" "Major mode for editing typescript"
-  '((emacs "24.3"))
-  :commit "b369d7d2518fa11760ac3908a383405350cd51d2" :keywords
-  '("typescript" "languages")
-  :url "http://github.com/ananthakumaran/typescript.el")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
